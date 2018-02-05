38d24748ab523e378af75d78c4dcb0c29f7e9df7
CYGXYPJL <251738930@qq.com>
2018-02-05 17:56:18 +0800

    音乐播放器
    优化界面

---
+++ BlackLeo/js/musicList.js
@@ -0,0 +1,17 @@
+function GetMusicList() {
+    var musicList = [
+        {"src":"FIELD OF VIEW-渐渐被你吸引.mp3","title":"FIELD OF VIEW-渐渐被你吸引"},
+        {"src":"Sound of My Dream.mp3","title":"Sound of My Dream"},
+        {"src":"倪莫问-九九八十一.mp3","title":"倪莫问-九九八十一"},
+        {"src":"倪莫问-牵丝戏.mp3","title":"倪莫问-牵丝戏"},
+        {"src":"刘珂矣-风筝误.mp3","title":"刘珂矣-风筝误"},
+        {"src":"夏天播放-小宝贝.mp3","title":"夏天播放-小宝贝"},
+        {"src":"张卫健-真英雄.mp3","title":"张卫健-真英雄"},
+        {"src":"悲怆第三章.mp3","title":"悲怆第三章"},
+        {"src":"戴荃-悟空.mp3","title":"戴荃-悟空"},
+        {"src":"林子祥-男儿当自强.mp3","title":"林子祥-男儿当自强"},
+        {"src":"风铃-莫失莫忘.mp3","title":"风铃-莫失莫忘"},
+    ];
+
+    return musicList;
+}
--- main.js
+++ main.js
@@ -1,1 +1,90 @@
-console.log('come on, my nodejs......');
+var http = require("http");
+var url = require("url");
+var fs = require("fs");
+var path = require("path");
+
+var types = {
+	"css": "text/css",
+	"gif": "image/gif",
+	"html": "text/html",
+	"ico": "image/x-icon",
+	"jpeg": "image/jpeg",
+	"jpg": "image/jpeg",
+	"js": "text/javascript",
+	"json": "application/json",
+	"pdf": "application/pdf",
+	"png": "image/png",
+	"svg": "image/svg+xml",
+	"swf": "application/x-shockwave-flash",
+	"tiff": "image/tiff",
+	"txt": "text/plain",
+	"mp3": "audio/mp3",
+	"wav": "audio/x-wav",
+	"wma": "audio/x-ms-wma",
+	"wmv": "video/x-ms-wmv",
+	"xml": "text/xml"
+};
+
+var server = http.createServer(function (req, res) {
+	var pathname = url.parse(req.url).pathname;
+	if (pathname.length == 1) pathname += "index.html";
+
+	pathname = decodeURI(pathname);
+
+	var realPath = path.join("BlackLeo", pathname);
+	var extname = path.extname(realPath);
+	extname = extname ? extname.slice(1) : "no";
+
+	fs.exists(realPath, function (exists) {
+		if (!exists) {
+			res.writeHead(404, {"Content-Type": "text/plain"});
+			res.write("This request URL " + pathname + " was not found on this server");
+			res.end();
+			return;
+		}
+
+		fs.readFile(realPath, "binary", function (err, file) {
+			if (err) {
+				res.writeHead(500, {"Content-Type": "text/plain"});
+				res.end(err);
+				return;
+			}
+
+			var type = types[extname] || "text/plain";
+			res.writeHead(200, {"Content-Type": type});
+			res.write(file, "binary");
+			res.end();
+		});
+	});
+});
+
+server.listen(8080);
+console.log("listening port " + 8080);
+
+function WriteMusicList() {
+	var dirPath = "./BlackLeo/music";
+	if (!fs.existsSync(dirPath)) return;
+
+	var state = fs.statSync(dirPath);
+	if (!state.isDirectory()) return;
+
+	var fileDataList = [];
+	var files = fs.readdirSync(dirPath);
+	files.forEach(function (file) {
+		if (file.match(/\.mp3/)) fileDataList.push({src: file, title: (file.split("."))[0]});
+	});
+
+	var data = "function GetMusicList() {\n";
+	data += "    var musicList = [\n";
+	fileDataList.forEach(function (fileData) {
+		data += "        " + JSON.stringify(fileData) + ",\n";
+	});
+	data += "    ];\n\n"
+	data += "    return musicList;\n" + "}";
+	
+	var filePath = "./BlackLeo/js/musicList.js";
+	fs.writeFileSync(filePath, data);
+}
+
+WriteMusicList();
+var updateMusicInterval = setInterval(WriteMusicList, 60000);
